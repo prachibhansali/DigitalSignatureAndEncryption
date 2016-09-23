a4af33c12519fcca15bdb1cd218ef840e7e0e884
Prachi Bhansali <prachibh@ccs.neu.edu>
2016-09-23 14:15:49 -0400

    multiclient server communication

--- ChatClient.py
+++ ChatClient.py
@@ -4,6 +4,7 @@ from threading import Thread
 from optparse import OptionParser
 
 def main():
+	# Read options and arguments from command line
 	parser = OptionParser()
 	parser.add_option("-i", "--sip", dest = "server_ip", help = "ip address of the server")
 	parser.add_option("-p", "--sp", dest = "server_port", help = "port where server is running")
@@ -15,29 +16,30 @@ def main():
 
 	server_address = (options.server_ip, int(options.server_port))
 
-	# Create a UDP socket
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-	# Send Greeting message to server
-	send_message = sock.sendto('GREETING', server_address)
+	try:
+		# Create a UDP socket
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		# Send Greeting message to server to establish connection
+		send_message = sock.sendto('GREETING', server_address)
+	except socket.error, exc:
+		print "Caught exception while creating client socket: %s" % exc
 
 	# Send and receive messages simultaneously to and from server
 	Thread(target=send_msg, args=(sock,server_address)).start()
-	Thread(target=rec_msg, args=(sock,)).start()
+	Thread(target=receive_msg, args=(sock,)).start()
 
 def send_msg(sock, server_address):
 	try:
 		while True:
 			client_message = raw_input();
-			print 'sending ' + client_message
 			send_message = sock.sendto(client_message, server_address)
 	except socket.error, exc:
 		print "Caught exception while sending client message: %s" % exc
 
-def rec_msg(sock):
+def receive_msg(sock):
 	try:
 		while True:
-			incoming_message,_ = sock.recvfrom(4096)
+			incoming_message,_ = sock.recvfrom(1024)
 			print incoming_message
 	except socket.error, exc:
 		print "Caught exception while receiving message from server: %s" % exc
--- ChatServer.py
+++ ChatServer.py
@@ -2,9 +2,10 @@ from optparse import OptionParser
 import sys
 import socket
 
-connected_clients = []
+active_clients = [] # stores addresses of connected clients
 
 def main():
+	# Read options and arguments from command line
 	parser = OptionParser()
 	parser.add_option("-p", "--sp", dest = "port", help = "port where server is running")
 
@@ -14,23 +15,26 @@ def main():
 		print 'usage: python ChatServer.py --sp <port>'
 		return
 
-	server_port = options.port
+	try:
+		# Create a UDP server socket
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		server_address = ('localhost', int(options.port))
 
-	# Create a UDP server socket
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	server_address = ('localhost', int(server_port))
-	sock.bind(server_address)
-	print 'Server Initialized...'
+		# Bind socket to server address
+		sock.bind(server_address)
+		print 'Server Initialized...'
+	except socket.error, exc:
+		print "Caught exception while creating server socket: %s" % exc
 
 	while True:
-		data, address = sock.recvfrom(4096)
+		data, address = sock.recvfrom(1024);
 		if data=='GREETING':
-			print 'Client Connected : %s' % str(address) 
-			connected_clients.append(address)
-			data, address = sock.recvfrom(4096)
-		format_message = '<From ' + str(address) + '>: ' + data
-		for client in connected_clients:
-			sock.sendto(format_message, client)
+			active_clients.append(address)
+		else:
+			format_message = '<From ' + str(address[0]) + ':' + str(address[1]) + '>: ' + data
+			# Broadcast message to all active clients
+			for client_address in active_clients:
+				sock.sendto(format_message, client_address)
 
 if __name__ == "__main__":
 	main()
